fbb56b4003fd0677e431f0803dc398c42074b4e2
gonpeche <espeche.g@gmail.com>
2026-04-28 22:02:45 -0300

    api data

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,1 +1,4 @@
-export const API_BASE_URL = "http://localhost:3001";
+const DEFAULT_API_BASE_URL = "http://localhost:3001";
+
+export const API_BASE_URL =
+  import.meta.env.VITE_API_BASE_URL ?? DEFAULT_API_BASE_URL;
