1af599f01881a566a44b312a4b3646ed1e9f2f8d
gonpeche <espeche.g@gmail.com>
2026-04-28 21:35:22 -0300

    default api

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,1 +1,6 @@
-export const API_BASE_URL = "http://localhost:3001";
+const DEFAULT_API_BASE_URL = "http://localhost:3001";
+
+const configuredApiBaseUrl =
+  import.meta.env.VITE_API_BASE_URL ?? DEFAULT_API_BASE_URL;
+
+export const API_BASE_URL = configuredApiBaseUrl.replace(/\/$/, "");
